0c6693567c2af63582b2509379377442a035a84e
D4rK7355608 <d4rk7355608@gmail.com>
2025-08-02 09:33:42 +0300

    Remove CleaningEventBusTest and CONTRIBUTING.md
    This commit removes the `CleaningEventBusTest.kt` file as the `CleaningEventBus` it was testing has been removed.
    It also deletes the `CONTRIBUTING.md` file.
    Additionally, JUnit and kotlinx-coroutines-test dependencies are removed from `app/build.gradle.kts` as they are no longer needed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -126,7 +126,4 @@ dependencies {
     implementation(dependencyNotation = libs.compressor)
     implementation(dependencyNotation = libs.coil3.coil.video)
     implementation(dependencyNotation = libs.apache.commons.compress)
-
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
 }
