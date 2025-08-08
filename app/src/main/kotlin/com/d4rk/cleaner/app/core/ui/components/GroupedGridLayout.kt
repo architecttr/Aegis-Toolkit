b044af84c388cfbf5bd54df3198e00dd58a21b1c
Mihai-Cristian Condrea <d4rk7355608@gmail.com>
2025-08-08 16:02:19 +0300

    feat(ui): add grouped grid layout component

---
+++ app/src/main/kotlin/com/d4rk/cleaner/app/core/ui/components/GroupedGridLayout.kt
@@ -0,0 +1,59 @@
+package com.d4rk.cleaner.app.core.ui.components
+
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import com.d4rk.android.libs.apptoolkit.core.utils.constants.ui.SizeConstants
+
+@Composable
+fun <T> GroupedGridLayout(
+    items: List<T>,
+    chunkSize: Int = 2,
+    itemContent: @Composable (T) -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(SizeConstants.MediumSize)
+            .animateContentSize(),
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(SizeConstants.LargeIncreasedSize)),
+        ) {
+            Column(verticalArrangement = Arrangement.spacedBy(SizeConstants.ExtraTinySize)) {
+                items.chunked(chunkSize).forEach { chunk ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .animateContentSize(),
+                        horizontalArrangement = Arrangement.spacedBy(
+                            space = SizeConstants.ExtraTinySize,
+                            alignment = Alignment.CenterHorizontally,
+                        ),
+                        verticalAlignment = Alignment.CenterVertically,
+                    ) {
+                        for (item in chunk) {
+                            itemContent(item)
+                        }
+                        if (chunk.size == 1) {
+                            Spacer(modifier = Modifier.weight(1f))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
